272530d780d92887a4ef68a288e4226f1924a8ae
monaAK <34164926+abdelker@users.noreply.github.com>
2023-03-17 17:11:35 +0100

    use if instead of cond

--- src/ontologies-manipulator.lisp
+++ src/ontologies-manipulator.lisp
@@ -10,36 +10,48 @@
 
 (defun init-ontologies-man ()
     "Constructs a manipulator for several instances of ontologies."
+
     (init-manager)
 )
 
 (defun wait-init (&optional (timeout -1))
     "Wait for ontologenius services to be advertised and available for. Blocks until it is.
-        timeout(int) is the amount of time to wait for before timing out.
-        If timeout is -1 (default), waits until the node is shutdown.
-        "
-(cond 
-    ((not (timeout -1))
-    
-        (wait-for-service "ontologenius/manage" timeout))
-    
-    (wait-for-service "ontologenius/manage")
-    
-    )
+    timeout(int) is the amount of time to wait for before timing out.
+    If timeout is -1 (default), waits until the node is shutdown."
+
+    (cond 
+        ((not (eql timeout -1))
+        
+            (roslisp:wait-for-service "ontologenius/manage" timeout))
+        
+        
+        ((roslisp:wait-for-service "ontologenius/manage"))
+        
+        )
 
 )
 
-(defun add (name)
+(defun add-onto (name)
     "Creates a new instance of ontologyManipulator identified by the name name(str).
     Returns False if the creation fails. Returns True even if the instance already exists."
-    (let ((onto-man (make-instance 'onto-man :name name)))   
-    (cond 
-        ((eq (member name 'manipulators) t)
-            (return t))
-    
-        (cond 
-            ((eq (add-manage name) nil)
-                (nil))
-            (((setf (gethash name *manipulators*) onto-man))))
 
-)))
+    (let ((onto-man (make-instance 'onto-man :name name)))
+            ;;(print onto-man)
+
+        (if (gethash name *manipulators*)
+                (values t)
+                
+                (if (eq (add-inst-onto name) nil)
+                    (values nil)
+
+                    (setf (gethash name *manipulators*) onto-man)
+                ))
+
+            ;; ((cond 
+            ;;     ((eq (add-inst-onto name) nil)
+            ;;         (values nil))))
+
+                ;;(((setf (gethash name *manipulators*) onto-man)))
+                )
+)
+
