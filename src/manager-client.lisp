4ec8fb013f360305cc12704fc377db85ce75bb93
monaAK <34164926+abdelker@users.noreply.github.com>
2023-04-21 15:11:42 +0200

    removed client from func call

--- src/manager-client.lisp
+++ src/manager-client.lisp
@@ -22,29 +22,30 @@
 (defun list-inst-ono ()
     "Returns the name of the instantiated ontologies (str[])."
 
-    (call *manager-client* "list" "")
+    (call "list" "")
 )
 
 (defun add-inst-onto (name)
     "Create an ontology instance named name(str).s
     Returns False if the service call fails."
    ;; (princ "here manager add")(terpri)
+   (init-manager)
     (call-nr "add" name)
 )
 
 (defun copy-inst-onto (dest-name src-name)
     "Create a copy of the ontology instance named src_name(str) with the name dest_name(str).
     Returns False if the service call fails or if the copy fails."
-
+    (init-manager)
     (let (( name (concatenate 'string dest-name "=" src-name)))   
-    (call-bool *manager-client* "add" name))
+    (call-bool  "add" name))
 )
 
 (defun delete-inst-onto (name)
     "Delete the instance of the ontology named name(str)
     Returns False if the service call fails."
-    
-    (call-nr *manager-client* "delete" name)
+    (init-manager)
+    (call-nr "delete" name)
 )
 
 (defun get-difference (onto1 onto2 concept)
@@ -52,7 +53,7 @@
         The elements of the returned vector are formated as : [+]concept_from|property|concept_on OR [-]concept_from|property|concept_on.
         An element is positive if it is present in onto_1 and not in onto_2 and negative in reverse.
         The difference in inheritance knowledge between concepts is returned with the property isA."
-
+    (init-manager)
     (let ((onto (concatenate 'string onto1 "|" onto2 "|" concept)))
-    (call-nr *manager-client* "difference" onto))
+    (call-nr  "difference" onto))
 )
